f3704176fb81d5bff2e8e16af4470eb7abc37f1e
junhkchoi <leisure1566@gmail.com>
2025-06-20 19:28:57 +0900

    30강 삼항 연산자

---
+++ src/cond/CondOp1.java
@@ -0,0 +1,10 @@
+package cond;
+
+public class CondOp1 {
+    public static void main(String[] args) {
+        int age = 18;
+        String status;
+
+        status = (age >= 20) ? "성인" : "미성년";
+    }
+}
