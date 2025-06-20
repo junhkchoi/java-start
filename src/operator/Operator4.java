3156d19bb1533bc20d9134116da055a54e137877
junhkchoi <leisure1566@gmail.com>
2025-06-20 18:06:35 +0900

    19강 연산자 우선순위

---
+++ src/operator/Operator4.java
@@ -0,0 +1,10 @@
+package operator;
+
+public class Operator4 {
+    public static void main(String[] args) {
+        int sum1 = 2 * 2 + 3 * 3;
+        int sum2 = (2 * 2) + (3 * 3);
+        System.out.println(sum1);
+        System.out.println(sum2);
+    }
+}
