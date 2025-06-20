9f325e1397bcc3e130fa020b56f2a7e79f24dc84
junhkchoi <leisure1566@gmail.com>
2025-06-20 14:26:13 +0900

    7강 주석

--- src/CommentJava.java
+++ src/CommentJava.java
@@ -2,7 +2,7 @@ public class CommentJava {
 
     // psvm으로 쉽게 메인메서드를 만들 수 있습니다.
     public static void main(String[] args) {
-        System.out.println("hellp java1"); // hello java1을 출력합니다
+        System.out.println("hellp java1");  // hello java1을 출력합니다
         // sout을 하면 간단하게 출력 메서드를 만들 수 있습니다.
 
         /*
