ced128062098bfe7a164d680e4e9adf27cd9cade
junhkchoi <leisure1566@gmail.com>
2025-06-20 15:31:36 +0900

    13강 변수타입 2

---
+++ src/variable/Var8.java
@@ -0,0 +1,20 @@
+package variable;
+
+public class Var8 {
+    public static void main(String[] args) {
+        // 정수
+        byte b = 127;
+        short s = 32767;
+        int i = 2147483647;
+        long l = 9223372036854775807L;
+
+        // 실수
+        float f = 3.14159265358979f;
+        double d = 3.14159265358979;
+    }
+    // 실무에선 숫자를 다룰때 int, long, double을 주로 씀
+    // boolean도 조건문에서 자주쓰고,
+    // char도 그냥 String으로 대체해서
+    // 메모리 절약하는 거 보다 개발 효율을 높이자!
+
+}
