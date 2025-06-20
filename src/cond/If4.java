01dd897516711398dfa1302e59c89c878eb431ff
junhkchoi <leisure1566@gmail.com>
2025-06-20 19:11:06 +0900

    28강 if문3

--- src/cond/If4.java
+++ src/cond/If4.java
@@ -2,6 +2,20 @@ package cond;
 
 public class If4 {
     public static void main(String[] args) {
+        // 조건이 여러개일때 항상 else if를 사용하는것은 아님!
+        // 독립적인 조건들을 검사해야할 때는 if문을 여러번 쓴다
 
+        int age = 9;
+        int price = 11000;
+        int discount = 0;
+
+        if (age <= 10) {
+            discount += 1000;
+        }
+        if (price >= 10000) {
+            discount += 1000;
+        }
+
+        System.out.println("할인금액: " + discount + "원");
     }
 }
