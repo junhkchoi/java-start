7a63d90c2e089b4b2076f0b5135fb05af76c237d
junhkchoi <leisure1566@gmail.com>
2025-06-20 18:44:45 +0900

    23강 대입 연산자

---
+++ src/operator/Assign2.java
@@ -0,0 +1,18 @@
+package operator;
+
+public class Assign2 {
+    public static void main(String[] args) {
+
+        int a = 5;
+
+        a += 3;
+        a -= 1;
+        a *= 2;
+        a /= 3;
+        a %= 5;
+
+        System.out.println("a = " + a);
+
+
+    }
+}
